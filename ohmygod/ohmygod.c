2e95dfc8a69aebaa73fcf35bf547b9ea274cf593
wseung123 <swch7338@naver.com>
2023-03-15 16:37:16 +0900

    초기 커밋.

---
+++ ohmygod/ohmygod.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<stdint.h>
+typedef struct dkyposition
+{
+
+
+	int xx;
+	int yy;
+	float zz;
+
+}dkupos;
+float sum1(struct dkyposition* aa)
+{
+
+	float rst = (aa->xx + aa->yy);
+	aa->zz = rst;
+	return rst;
+
+}
+int main(void)
+{
+	struct dkyposition xypos;
+	xypos.xx = 10;
+	xypos.yy = 20;
+	sum1(&xypos);
+	printf("x:%d, y:%d\r\n", xypos.xx, xypos.yy);
+	printf("sum: % d\r\n", sum1);
+		
+	return 0;
+
+
+
+}
